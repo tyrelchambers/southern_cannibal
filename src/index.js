5177aa3618fdfafde7cd1200a0ba10edc69c9875
Tyrel Chambers <tychambers3@gmail.com>
2019-06-09 16:36:00 -0400

    add logrocket

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SubmitStory from './sections/SubmitStory/SubmitStory';
-
+import LogRocket from 'logrocket';
+LogRocket.init('kstoxh/canni');
 ReactDOM.render(
   <Router>
     <Switch>
