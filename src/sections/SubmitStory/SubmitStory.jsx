64c5305a44c34e27ac7a4a4f45eb25bc8e00c94a
Tyrel Chambers <tychambers3@gmail.com>
2023-09-26 18:28:06 -0400

    update text

--- src/sections/SubmitStory/SubmitStory.jsx
+++ src/sections/SubmitStory/SubmitStory.jsx
@@ -9,8 +9,11 @@ export default function SubmitStory() {
       <h1 className="styled-title section">Submit a story</h1>
       <p className="sub-text">
         By submitting your story, you agree to allow Southern Cannibal the
-        commercial use of the story typed herein, to edit or change for use on
-        the Southern Cannibal YouTube Channel.
+        commercial use of the story typed herein, to edit or change and publish
+        on all Southern Cannibal related content. If your story is used in any
+        southern cannibal content there are no repeals. The content will NOT be
+        removed just because you change your mind. Please keep that in mind when
+        submitting your story.
       </p>
       <SubmitForm />
     </div>
